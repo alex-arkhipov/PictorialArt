f4f1b8a68ac8f3f9fc4eb8326a4a32adf9c1c509
alex-arkhipov <simeon.arkhipov@gmail.com>
2018-02-04 19:30:12 +0300

    Initial comit

---
+++ PictorialArt/src/main/java/com/alexarkhipov/works/pictorialart/PictorialArtApplication.java
@@ -0,0 +1,12 @@
+package com.alexarkhipov.works.pictorialart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictorialArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictorialArtApplication.class, args);
+	}
+}
