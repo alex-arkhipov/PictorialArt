5acfeaafa00e999fb9b6c74afa696a742391acf2
Alex Arkhipov <simeon.arkhipov@gmail.com>
2018-03-03 12:00:16 +0300

    Added UTF-8 charset for DB

--- PictorialArt/src/main/resources/schema.sql
+++ PictorialArt/src/main/resources/schema.sql
@@ -13,11 +13,11 @@ CREATE TABLE Author (
 	BYear int,
 	DYear int,
 	Sex tinyint not null
-);
+) DEFAULT CHARSET=utf8;
 
-CREATE TABLE Genre (id int primary key not null auto_increment, Genre varchar(100) not null);
+CREATE TABLE Genre (id int primary key not null auto_increment, Genre varchar(100) not null) DEFAULT CHARSET=utf8;
 
-CREATE TABLE Description (id int primary key auto_increment, Description text not null);
+CREATE TABLE Description (id int primary key auto_increment, Description text not null) DEFAULT CHARSET=utf8;
 
 CREATE TABLE Production (
 	id int primary key auto_increment,
@@ -28,9 +28,9 @@ CREATE TABLE Production (
 	GenreID int,
 	Filename varchar(100),
 	Popularity tinyint
-);
+) DEFAULT CHARSET=utf8;
 ALTER TABLE Production ADD FOREIGN KEY (AuthorID) REFERENCES Author(id);
 ALTER TABLE Production ADD FOREIGN KEY (GenreID) REFERENCES Genre(id);
 ALTER TABLE Production ADD FOREIGN KEY (DescriptionID) REFERENCES Description(id);
 
-CREATE TABLE Player (id int primary key auto_increment, Nickname varchar(20) not null, Name varchar(255) not null, Sex tinyint not null, Hash varchar(255) not null, Age int, LastAccess date, Scores int, Email varchar(100) not null);
+CREATE TABLE Player (id int primary key auto_increment, Nickname varchar(20) not null, Name varchar(255) not null, Sex tinyint not null, Hash varchar(255) not null, Age int, LastAccess date, Scores int, Email varchar(100) not null) DEFAULT CHARSET=utf8;
